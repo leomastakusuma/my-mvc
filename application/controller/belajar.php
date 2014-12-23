f305ec65a102604270ad8c779c6683b0a7672460
Deni Permana <deni@duniakreatif.com>
2014-12-23 16:57:27 +0700

    update

---
+++ application/controller/belajar.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Belajar extends Controller{
+	public function index(){
+		echo 'Absd';
+	}
+
+	public function Simple(){
+		echo 'Simpel';
+	}
+}
---
+++ application/controller/index.php
@@ -0,0 +1,11 @@
+<?php
+
+class Index extends Controller{
+
+	public function index(){
+		echo 'test';
+	}
+	public function testing(){
+		echo 'testing';
+	}
+}
